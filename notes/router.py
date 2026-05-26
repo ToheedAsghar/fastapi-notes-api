4b166ce7bf5ae409fb79fa839cf111601d290ac5
Toheed Asghar <toheedasghar1@gmail.com>
2026-05-26 19:38:50 +0500

    added notes-router

--- notes/router.py
+++ notes/router.py
@@ -1,29 +1,49 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, status
 
+from database import get_db
+from notes.models import Note
+from notes.storage import filter_notes, get_note_or_404, toggle_archive, delete_note, update_note, create_note
 from notes.schemas import NoteCreate, NoteResponse, NoteUpdate
 
 router = APIRouter(prefix="/notes", tags=["notes"])
 
 @router.get("", response_model=NoteResponse)
-def list_notes_endpoint():
-    ...
+def list_notes_endpoint(notes: Annotated[list[Note], Depends(filter_notes)]):
+    return notes
 
 @router.get("/{note_id}", response_model=NoteResponse)
-def get_note_endpoint():
-    ...
+def get_note_endpoint(
+    note: Annotated[Note, Depends(get_note_or_404)],
+):
+    return note
 
 @router.post("", response_model=NoteResponse)
-def create_note_endpoint():
-    ...
+def create_note_endpoint(
+    note: NoteCreate,
+    db: Annotated[Session, Depends(get_db)]
+):
+    return create_note(note, None, db)
 
 @router.put("/{note_id}", response_model=NoteUpdate):
-def update_note_endpoint():
-    ...
+def update_note_endpoint(
+        note: Annotated[Note, Depends(get_note_or_404)],
+        updated_note: NoteCreate,
+        db: Annotated[Session, Depends(get_db)]
+):
+    return update_note(note, updated_note, db)
 
 @router.delete("/{note_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_note_endpoint():
-    ...
+def delete_note_endpoint(
+    note: Annotated[Note, get_note_or_404],
+    db: Annotated[Session, Depends(get_db)]
+):
+    return delete_note(note, db)
 
 @router.patch("/{note_id}", response_model=NoteUpdate)
-def toggle_archieve():
-    ...
+def toggle_archieve(
+    note: Annotated[Note, get_note_or_404],
+    db: Annotated[Session, Depends(get_db)]
+):
+    return toggle_archive(note, db)
---
+++ notes/storage.py
@@ -0,0 +1,74 @@
+from fastapi import Depends, HTTPException, Query
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+from database import get_db
+from users.models import User
+from notes.models import Note
+from notes.schemas import NoteCreate, NoteResponse, NoteUpdate
+
+def get_note_or_404(
+        note_id: int,
+        db: Annotated[Session, Depends(get_db)]
+) -> Note:
+    note = db.query(Note).filter(Note.id == note_id).first()
+
+    if note is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return note
+
+def create_note(
+        note_data: NoteCreate,
+        current_user: User,
+        db: Session,
+) -> Note:
+    new_note = Note(**note_data.model_dump(), owner_id = current_user.id)
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    return new_note
+
+def delete_note(
+        note: Note,
+        db: Session
+) -> None:
+    db.delete(note)
+    db.commit() 
+
+def update_note(
+        note: Note,
+        updated_note: NoteCreate,
+        db: Session,
+) -> Note:
+    for field, value in updated_note.model_dump().items():
+        setattr(note, field, value)
+    
+    db.commit()
+    db.refresh(note)
+    return note
+
+def toggle_archive(
+        note: Note,
+        db: Session,
+) -> Note:
+    note.is_arhived = not bool(note.is_arhived)
+    db.commit()
+    db.refresh(note)
+    return note
+
+def filter_notes(
+        db: Session,
+        title: str = Query(default=""),
+        skip: int = Query(default=0, ge=0),
+        limit: int = Query(default=10, ge=1, le=20)
+) -> list[Note]:
+    
+    query = db.query(Note)
+
+    if title is not None:
+        query = query.filter(Note.title.ilike(f"%{title}%"))
+    
+    return query.offset(skip).limit(limit).all()
+
+def get_no_of_books(db: Session):
+    return db.query(Note).count()
